b6eb130d39820fae7d37a83310214e34c421d7b4
pnoker <peter-no@foxmail.com>
2016-11-14 22:55:26 +0800

    添加通用读取文件流格式

--- src/com/dact/util/OperateTxtUtil.java
+++ src/com/dact/util/OperateTxtUtil.java
@@ -3,10 +3,12 @@ package com.dact.util;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
@@ -14,9 +16,9 @@ import java.util.ArrayList;
 public class OperateTxtUtil {
 	public ArrayList<String> readLine(String path) {
 		ArrayList<String> line = new ArrayList<String>();
-		File file = new File(path);
 		try {
-			FileReader fileReader = new FileReader(file);
+			FileInputStream file = new FileInputStream(path);
+			InputStreamReader fileReader = new InputStreamReader(file, "utf-8");
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 			String temp;
 			while ((temp = bufferedReader.readLine()) != null) {
