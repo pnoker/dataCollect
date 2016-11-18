8e3312c55db587c175e35feeaa30461a3dcc2a61
pnoker <peter-no@foxmail.com>
2016-11-18 15:10:34 +0800

    北京

--- src/com/dact/init/Initial.java
+++ src/com/dact/init/Initial.java
@@ -2,10 +2,6 @@ package com.dact.init;
 
 public class Initial {
 	public void init() {
-//		Weihao weihao = new Weihao();
-//		weihao.initWeihao();
-		Wireless wireless = new Wireless();
-		wireless.initWireless();
 		Shui shui = new Shui();
 		shui.initShui();
 	}
